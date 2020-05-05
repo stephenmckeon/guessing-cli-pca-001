88aabf3cfaa817ca806d0811ba8b60c90f84e870
Stephen McKeon <stephen.mckeon@powerhrg.com>
2020-05-05 13:22:46 -0400

    Done.

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,14 @@
-# Code your solution here!
+def run_guessing_game
+  num = rand(6) + 1
+
+  user_input = gets.strip
+
+  if user_input == "exit"
+    puts "Goodbye!"
+    nil
+  elsif user_input == "#{num}"
+    puts "You guessed the correct number!"
+  else
+    puts "Sorry! The computer guessed #{num}."
+  end
+end
